332eea9c1811e1aa38e04a33704f59246bebbb4b
dotmaker619 <dotmaker619@outlook.com>
2022-01-28 20:19:25 +0900

    add search function

--- src/views/Explore/components/Rows.vue
+++ src/views/Explore/components/Rows.vue
@@ -3,6 +3,7 @@
     <div class="w-full lg:mt-o mt-4 bg-gray-900 border border-gray-700 p-4 items-center rounded-2xl overflow-hidden flex flex-col wow fadeInDown mb-4" data-wow-duration="0.3s" data-wow-delay="0.6s">
       <h3 class="gradient-text">Filter by Token</h3>
       <input
+        v-model="searchValue"
         class="bg-launchpad_primary overline bg-opacity-10 hover:shadow-launchpad_primary focus:shadow-launchpad_primary relative filterByToken mt-4 border-launchpad_primary transition-all duration-200 placeholder-launchpad_primary-light text-launchpad_primary rounded-full border p-3 w-full text-center"
         placeholder="Search by token name or address"
       />
@@ -24,7 +25,7 @@
               </div>
             </div>
             <div class="h-154 overflow-scroll px-1 py-3 lg:py-3 lg:px-3">
-              <div v-for="launch in launches" :key="launch.tokenAddr">
+              <div v-for="launch in filteredCards" :key="launch.tokenAddr">
                 <Rowsingle :id="launch.tokenAddr" :isLive="launch.isLive" :icon="launch.icon" :isOwned="launch.isOwned" :isPublic="launch.isLive" :name="launch.tokenName" :ratio="launch.rate" :participants="launch.participants" :launchType="this.launch_type" :partnerType="launch.partnerType" :progress="launch.soldTokens*100/launch.presaleTokens" :startDate="launch.startTime" :endDate="launch.endTime" />
               </div>
             </div>
@@ -53,11 +54,23 @@ export default {
   },
   data() {
     return {
-      launch_type: "Presale"
+      launch_type: "Presale",
+      searchValue: ""
     }
   },
   computed: {
     ...mapState('launchpad', ['launches']),
+    filteredCards() {
+      let launches = [ ...this.launches ];
+
+      if (this.searchValue != "" && this.searchValue) {
+        launches = this.launches.filter((launch) => {
+          return launch.tokenName.toUpperCase().includes(this.searchValue.toUpperCase());
+        });
+      }
+
+      return launches;
+    }
   },
 };
 </script>
