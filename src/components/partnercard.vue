f41803351e1cf7a310ef8dc3f93036034d48ed30
developsuper <grumpy.pretty@gmail.com>
2022-02-05 07:32:22 +0900

    fix partner cards

--- src/components/partnercard.vue
+++ src/components/partnercard.vue
@@ -1,34 +1,40 @@
 <template>
-  <div :key="partner_type.id" @click="$router.push(`/partners/${partner_type.id}`)" class="w-full bg-gray-900 p-6 shadow-smooth border-gray-600 border rounded-2xl h-full flex flex-col items-center">
-        <img class="mb-4 bg-opacity-20 w-32 h-32 rounded-full" :src="partner_type.icon" alt="Logo" />
-          <h3 class="text-center gradient-text lg:h-16 xl:h-8">{{ partner_type.name }}</h3>
-          <p class="custom caption p-4 mb-2 mt-4 max-w-100 text-center lg:h-32 xl:h-24">{{ partner_type.description }}</p>
-          <div class="flex flex-row space-x-2 mb-2 mt-5">
-            <a href="https://mobile.twitter.com/olympus_token" target="_blank">
-                <div class="social h-10 w-10 rounded-full text-center pt-1.5"><i class="fab fa-twitter"></i></div>
-            </a>
-            <a href="https://www.instagram.com/olympusbsc/?hl=en" target="_blank">
-                <div class="social h-10 w-10 rounded-full text-center pt-1.5"><i class="fab fa-instagram"></i></div>
-            </a>
-            <a href="https://t.me/OlympusOfficial" target="_blank">
-                <div class="social h-10 w-10 rounded-full text-center pt-1.5"><i class="fab fa-telegram"></i></div>
-            </a>
-            <a href="mailto:contact@olympustoken.io" target="_blank">
-                <div class="social h-10 w-10 rounded-full text-center pt-1.5"><i class="fa fa-envelope"></i></div>
-            </a>
-            </div>
-            <div class="py-1 transition-colors duration-200 px-6 text-base bg-gray-600 hover:bg-gray-500 font-semibold mt-4 rounded-full">VIEW LAUNCHES</div>
-          
+  <div 
+    @click="$router.push(`/partners/${partner_type.id}`)" 
+    class="relative h-full w-full bg-gray-900 px-6 py-8 shadow-smooth border-gray-600 border rounded-2xl flex flex-col items-center justify-between"
+  >
+    <div class="mb-4 flex flex-col items-center">
+      <img
+        class="mb-4 bg-opacity-20 w-32 h-32 rounded-full" 
+        :src="partner_type.icon" alt="Logo" 
+      />
+      <h3 class="text-center gradient-text lg:h-12 xl:h-8">{{ partner_type.name }}</h3>
+      <p class="custom caption p-4 mb-2 mt-2 max-w-100 text-center lg:h-16 xl:h-14">{{ partner_type.description }}</p>
+    </div>
+    <div class="flex flex-col items-center mb-6">
+      <div class="flex flex-row space-x-2 mb-2 mt-5">
+        <a href="https://mobile.twitter.com/olympus_token" target="_blank">
+            <div class="social h-10 w-10 rounded-full text-center pt-1.5"><i class="fab fa-twitter"></i></div>
+        </a>
+        <!-- <a href="https://www.instagram.com/olympusbsc/?hl=en" target="_blank">
+            <div class="social h-10 w-10 rounded-full text-center pt-1.5"><i class="fab fa-instagram"></i></div>
+        </a> -->
+        <a href="https://t.me/OlympusOfficial" target="_blank">
+            <div class="social h-10 w-10 rounded-full text-center pt-1.5"><i class="fab fa-telegram"></i></div>
+        </a>
+        <!-- <a href="mailto:contact@olympustoken.io" target="_blank">
+            <div class="social h-10 w-10 rounded-full text-center pt-1.5"><i class="fa fa-envelope"></i></div>
+        </a> -->
+        </div>
+      <div class="py-1 transition-colors duration-200 px-6 text-base bg-gray-600 hover:bg-gray-500 font-semibold mt-4 rounded-full">VIEW LAUNCHES</div>
+    </div>
   </div>
 </template>
 <script>
 
-import TimeLine from "@/components/TimeLine.vue";
-
 export default {
   name: 'PartnerCard',
   components: {
-    TimeLine,
   },
   props: {
     partner_type: Object,
--- src/views/partners/components/partnerslide.vue
+++ src/views/partners/components/partnerslide.vue
@@ -9,9 +9,9 @@
     :breakpoints="swiperOptions.breakpoints"
   >
     <swiper-slide v-for="partner_type in partner_types" :key="partner_type.id">
-        <PartnerCard 
-            :partner_type="partner_type" 
-        />
+      <PartnerCard 
+        :partner_type="partner_type" 
+      />
     </swiper-slide>
   </swiper>
 </template>
@@ -21,10 +21,9 @@ import SwiperCore, { Navigation, Pagination, A11y } from "swiper";
 
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from "swiper/vue";
-import TimeLine from "@/components/TimeLine.vue";
-import PartnerCard from '@/components/partnercard.vue';
+import PartnerCard from '@/components/PartnerCard.vue';
 import { useWindowSize } from 'vue-window-size';
-import { mapGetters, mapState } from 'vuex';
+import { mapState } from 'vuex';
 
 // Import Swiper styles
 import "@/assets/scss/main.scss";
@@ -39,7 +38,6 @@ export default {
   components: {
     Swiper,
     SwiperSlide,
-    TimeLine,
     PartnerCard
   },
   data() {
